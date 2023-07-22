81eb3ee56399dcf0f6192994782e8d93dd958f70
Deepam GitHub <deepam99h@gmail.com>
2023-07-23 00:48:21 +0530

    Lecture-6(ll)

--- Lecture-6(ll)-cipherschool.cpp
+++ Lecture-6(ll)-cipherschool.cpp
@@ -1,11 +1,25 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
+
 int main(){
-    int n;
-    cout<<"enter the no. upto u want to print "<<endl;
-    cin>>n;
-    for(int i=0; i<=n ; i++){
-        cout<<i<<" ";
-    }
-  return 0;
+	//	int i,j,k;
+	//	i=0;
+	//	j=0;
+	//	k=0;
+	//	
+	//	cout<<i<<" "<<j<<" "<<k<<endl;
+	//	j = i++;
+	//	cout<<i<<" "<<j<<" "<<k<<endl;
+	//	k = ++i;
+	//	
+	//	cout<<i<<" "<<j<<" "<<k<<endl;
+	//	cout<<++i<<endl;
+	//	cout<<i;
+
+    int i;
+    for(i=1;i<101;i++) //called the update which happend after the iteratiion(part of a loop is completed)
+	{
+    	cout<<i<<" ";
+	}
+	return 0;
 }
